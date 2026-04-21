023fa2be8c26372e403d4404ba709afea5111fc1
DxRxN <serbuscadorin@gmail.com>
2026-04-21 13:49:56 +0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+function mobileMenu() {
+    var x = document.getElementsByTagName("navbar")[0];
+    if (x.className === "") {
+        x.className += "mobile";
+    } else {
+        x.className = "";
+    }
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+   
+    // Ascunde toate imaginile
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+   
+    slideIndex++;
+   
+    if (slideIndex > slides.length) {slideIndex = 1}    
+   
+    if (slides[slideIndex-1]) {
+        slides[slideIndex-1].style.display = "block";  
+    }
+   
+    // Programează următoarea schimbare peste 3000 de milisecunde (3 secunde)
+    setTimeout(showSlides, 3000);
+}
